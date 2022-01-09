e526d5ec0968f8709ea93e61c0ab937799406843
CSopiko <skurd18@freeuni.edu.ge>
2022-01-10 02:59:01 +0400

    Add receipt folder

---
+++ app/core/__init__.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+from app.core.cashier.interactor import (
+    CloseReceiptRequest,
+    OpenReceiptRequest,
+    OpenReceiptResponse,
+)
+from app.core.receipt.interactor import (
+    AddItemRequest,
+    FetchReceiptRequest,
+    FetchReceiptResponse,
+    IReceiptRepository,
+    ReceiptInteractor,
+)
+
+
+@dataclass
+class StoreCore:
+    receipt_interactor: ReceiptInteractor
+
+    def open_receipt(self, request: OpenReceiptRequest) -> OpenReceiptResponse:
+        return self.receipt_interactor.open_receipt(request)
+
+    def add_item(self, request: AddItemRequest) -> None:
+        self.receipt_interactor.add_item(request)
+
+    def fetch_current_receipt(
+        self, request: FetchReceiptRequest
+    ) -> FetchReceiptResponse:
+        return self.receipt_interactor.fetch_open_receipt(request)
+
+    def close_receipt(self, request: CloseReceiptRequest) -> None:
+        self.receipt_interactor.close_receipt(request)
+
+    def x_report(self, date: str) -> None:
+        pass
+
+    @classmethod
+    def create(cls, receipt_repository: IReceiptRepository) -> "StoreCore":
+        return cls(
+            receipt_interactor=ReceiptInteractor(receipt_repository=receipt_repository)
+        )
