5b665eb9184cbe6db7fe603a6c3c4628d1516f23
CSopiko <skurd18@freeuni.edu.ge>
2022-01-10 02:58:21 +0400

    Update setup

--- app/runner/setup.py
+++ app/runner/setup.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 
+from app.core import IReceiptRepository, StoreCore
 from app.infra.api import receipt_api
+from app.infra.in_memory import ReceiptRepository
 
 
 def setup() -> FastAPI:
     app = FastAPI()
     app.include_router(receipt_api)
+    app.state.core = StoreCore.create(receipt_repository=setup_receipt_repository())
     return app
+
+
+def setup_receipt_repository() -> IReceiptRepository:
+    repository = ReceiptRepository()
+    return repository
